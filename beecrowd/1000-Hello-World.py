e89cc52637edb04a7da7291bedd31f1845d76edd
20261ctb0100001-crypto <20261ctb0100001@estudantes.ifpr.edu.br>
2026-04-09 20:11:01 +0000

    first commit

---
+++ beecrowd/1000-Hello-World.py
@@ -0,0 +1,17 @@
+'''
+Problema: beecrowd | 1000
+Data: ano.mes.dia
+Estudante: Exemplo fornecido pelo Profe. Berssa
+'''
+#Objetivo: Exibir exatamente a mensagem "Helloo Wold!" na tela
+
+#--- ANÀLISE (LIAC)---
+# Entrada : nenhuma -este problema não requer leitura de dados
+#Processamneto: nenhum - não há cálculo ou tranformação
+# Saída: exibir o texto " Hello Wold!' exatemente como especificado
+
+# print () é o comando de saída em Pyton
+# Equivalente ao bloco "diga ..." do Scratch
+
+
+print('Hello World!')
